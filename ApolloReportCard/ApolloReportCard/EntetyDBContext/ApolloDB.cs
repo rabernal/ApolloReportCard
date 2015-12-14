cd9c35052bc5e79d7519ba8973bb6abdc3d980b9
rabernal <bernal011584@gmail.com>
2015-12-14 14:46:14 -0700

    berst

---
+++ ApolloReportCard/ApolloReportCard/EntetyDBContext/ApolloDB.cs
@@ -0,0 +1,18 @@
+﻿using ApolloReportCard.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ApolloReportCard.EntetyDBContext
+{
+    public class ApolloDB : DbContext
+    {
+        public ApolloDB() : base("name=DefaultConnection")
+        {
+
+        }
+        public DbSet<CriteriaModel> Criteria { get; set; }
+    }
+}
---
+++ ApolloReportCard/ApolloReportCard/Models/CriteriaModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApolloReportCard.Models
+{
+    public class CriteriaModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string QuarterOneGrade { get; set; }
+        public string QuarterOneComments { get; set; }
+        public string QuarterTwoGrade { get; set; }
+        public string QuarterTwoComments { get; set; }
+        public string QuarterThreeGrade { get; set; }
+        public string QuarterThreeComments { get; set; }
+        public string QuarterFourGrade { get; set; }
+        public string QuarterFourComments { get; set; }
+    }
+}
